9f62ea5a5e022bce7c99a19ce9e241049f5d46d5
Victoria Smirnova <victoria.y.smirnova@gmail.coml>
2022-06-17 18:30:25 +0300

    feature added popup

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
-const editButton = document.querySelector('.editButton');
-const popup = document.querySelector('.popup');
-const popupCloseButton = popup.querySelector('.popup__close');
+let editButton = document.querySelector('.editButton');
+let popup = document.querySelector('.popup');
+let popupCloseButton = document.querySelector('.popup__close');
+let popupSave = document.querySelector('.popup__save');
 
-function openPopup(event){
-  popup.classList.add('popup_opened');
+let formElement = document.querySelector('.popup__form')
+let nameInput = formElement.querySelector('.popup__name')
+let occupationInput = formElement.querySelector('.popup__occupation')
+
+let profileName = document.querySelector('.profile__name')
+let profileOccupation = document.querySelector('.profile__occupation')
+
+function openPopup(evt){
+  popup.classList.add('popup__opened');
+  nameInput.value = profileName.textContent;
+  occupationInput.value = profileOccupation.textContent;
 }
 
-function closePopup(event){
-  popupCloseButton.classList.add('popup_closed');
+function closePopup(evt){
+  popup.classList.remove('popup__opened');
 }
 
-editButton.addEventListener('click', openPopup);
-editButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup)
+popupCloseButton.addEventListener('click', closePopup)
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = `${nameInput.value}`;
+  profileOccupation.textContent = `${occupationInput.value}`;
+  closePopup();
+}
+
+popupSave.addEventListener('click', formSubmitHandler);
+popupSave.addEventListener('submit', formSubmitHandler);
