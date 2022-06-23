1ba25e7d95eebb60a8a023e1c8c6cdd94b66e1bf
Victoria Smirnova <victoria.y.smirnova@gmail.coml>
2022-06-23 16:59:41 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
-let editButton = document.querySelector('.edit-button');
+let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupContainer = document.querySelector('.popup__container');
 let popupCloseButton = document.querySelector('.popup__close');
 let popupSave = document.querySelector('.popup__save');
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__name');
-let occupationInput = formElement.querySelector('.popup__occupation');
+let nameInput = formElement.querySelector('.popup__form_info_name');
+let occupationInput = formElement.querySelector('.popup__form_info_occupation');
 
 let profileName = document.querySelector('.profile__name');
 let profileOccupation = document.querySelector('.profile__occupation');
 
-function openPopup(evt) {
-  popup.classList.add('popup__opened');
+function openPopup() {
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   occupationInput.value = profileOccupation.textContent;
 }
 
-function closePopup(evt) {
-  popup.classList.remove('popup__opened');
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
 
 editButton.addEventListener('click', openPopup);
@@ -26,23 +26,22 @@ popupCloseButton.addEventListener('click', closePopup);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  profileName.textContent = `${nameInput.value}`;
-  profileOccupation.textContent = `${occupationInput.value}`;
+  profileName.textContent = nameInput.value;
+  profileOccupation.textContent = occupationInput.value;
   closePopup();
 }
 
-popupSave.addEventListener('click', formSubmitHandler);
-popupSave.addEventListener('submit', formSubmitHandler);
+popupContainer.addEventListener('submit', formSubmitHandler);
 
 
-popup.addEventListener('click',(event) => {
-  if (!event.defaultPrevented) {
-    closePopup();
-  }
-})
+// popup.addEventListener('click',(event) => {
+//   if (!event.defaultPrevented) {
+//     closePopup();
+//   }
+// })
 
-popupContainer.addEventListener('click', (event) => {
-  event.preventDefault();
-})
+// popupContainer.addEventListener('click', (event) => {
+//   event.preventDefault();
+// })
 
 
